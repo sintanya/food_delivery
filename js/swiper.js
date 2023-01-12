b084423125e48f60661ff7fc256be69b9cff9d0e
Tanya <tanja.synjak.98@ukr.net>
2023-01-12 22:37:04 +0200

    add specialities (+html benefits)

--- js/swiper.js
+++ js/swiper.js
@@ -7,6 +7,16 @@ const swiper = new Swiper('.card-slider', {
           nextEl: ".card-slider__button--right",
           prevEl: ".card-slider__button--left",
         },
+});
 
-   
+
+const swiper1 = new Swiper('.logo-slider', {
+     // Default parameters
+    slidesPerView: 8,
+    spaceBetween: 24,
+    loop: true,
+    navigation: {
+          nextEl: ".logo-slider__button--right",
+          prevEl: ".logo-slider__button--left",
+        },
 });
